2fb960b3bcfca99a0392b307d92580a3034bff2b
unknown <ec24313@qmul.ac.uk>
2025-03-28 12:06:46 +0000

    HelloWorld program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        System.out.println("Hello, Copy2!");
->>>>>>> new2
+        System.out.println("Hello, main!");
     }
 }
